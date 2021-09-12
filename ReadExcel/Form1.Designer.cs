fd90b5ee30d9f6c57fe37bc439561c2a6959af4c
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-12 13:21:17 -0300

    Fisrt commit

---
+++ ReadExcel/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ReadExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPlan = new System.Windows.Forms.ComboBox();
+            this.btRead = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbPlan
+            // 
+            this.cbPlan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPlan.FormattingEnabled = true;
+            this.cbPlan.Location = new System.Drawing.Point(667, 417);
+            this.cbPlan.Name = "cbPlan";
+            this.cbPlan.Size = new System.Drawing.Size(121, 21);
+            this.cbPlan.TabIndex = 0;
+            this.cbPlan.SelectedIndexChanged += new System.EventHandler(this.cbPlan_SelectedIndexChanged);
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(586, 415);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(75, 23);
+            this.btRead.TabIndex = 1;
+            this.btRead.Text = "button1";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(776, 397);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.cbPlan);
+            this.Name = "Form1";
+            this.Text = "Read Excel File";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPlan;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ ReadExcel/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+
+namespace ReadExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTableCollection tableCollection;
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel 97-2003 Workbook|*.xls|Excel Workbook|*.xlsx" } )
+            {
+                if (ofd.ShowDialog().Equals(DialogResult.OK))
+                {
+                    using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                            });
+
+                            tableCollection = result.Tables;
+
+                            cbPlan.Items.Clear();
+                            foreach (DataTable table in tableCollection)
+                            {
+                                cbPlan.Items.Add(table.TableName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void cbPlan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = tableCollection[cbPlan.SelectedItem.ToString()];
+            dataGridView.DataSource = dt;
+        }
+    }
+}
